470227af3dae8f60c31508daecaccdacc8210dac
Ali <alicewulfic@gmail.com>
2022-08-29 10:05:27 -0300

    Testing stuff

--- main.ino
+++ main.ino
@@ -1,11 +1,31 @@
+#include <Servo.h>
 #include <Otto.h>
 
-Otto Miku;
+#define STANDBY 15
+
+Servo serv0;
+
+void saludar(Servo);
 
 void setup(){
-    
+    serv0.attach(6);
 }
 
 void loop(){
+    saludar(serv0);
+}
 
+void saludar(Servo serv){
+    int i;
+
+    serv.write(STANDBY);
+
+    for(i=0; i<2; i++){
+        delay(1000);
+        serv.write(175);
+        delay(1000);
+        serv.write(135);
+    }
+
+    serv.write(STANDBY);
 }
