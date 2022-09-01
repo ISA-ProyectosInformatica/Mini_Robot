3e2177b6d7c2b7cf1df570f3a163044b4f3b7134
Ali <alicewuflric@gmail.com>
2022-09-01 15:41:07 -0300

    Ya esta todo probado, baila y todo <3, obviamente usamos codigo de la repo original. El link deberia estar en el README original.

--- main.ino
+++ main.ino
@@ -3,49 +3,41 @@
 
 #define STANDBY 15
 
-Servo serv0;
-Servo serv1;
-Servo serv2;
-Servo serv3;
-Servo serv4;
+Servo servs[6];
 
-Servo servs[5];
-
-void saludar(Servo);
 void home_custom();
 
 void setup(){
-    for(int i =2; i<=6; i++)
+    for(int i=2; i<=6; i++)
         servs[i].attach(i);
     pinMode(8, INPUT_PULLUP);
 }
 
 void loop(){
-    int state;
-    state = digitalRead(8);
 
-    if(state == LOW)
-        move_90(servs);
-        delay(1000);
+    if(digitalRead(8)== LOW){
         home_custom(servs);
-    /*serv0.write(0);
-    serv1.write(0);
-    serv2.write(0);
-    serv3.write(0);
-    serv4.write(0);
-    delay(1500);*/
+    }
 }
 
 void home_custom(Servo[]){
-    for(int i=0; i<=5; i++)
+    int i;
+
+    for(i=0;i<=6; i++)
+        servs[i].write(180);
+    delay(1000);
+
+    for(i=0; i<=6; i++)
         servs[i].write(0);
     delay(1000);
 
-    for(int i=0; i<=5; i++)
-        servs[i].write(0);
+    for(i=0; i<=6; i++)
+        servs[i].write(90);
+
+    delay(1000);
 }
 
 void move_90(Servo[]){
-    for (int i=0; i<=5; i++)
+    for (int i=0; i<=6; i++)
         servs[i].write(90);
 }
